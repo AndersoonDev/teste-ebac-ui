1160640a193ca94a57777f8310b3cbaa0faf5ee2
Anderson Silva <andynho1987@gmail.com>
2023-10-20 17:01:54 -0300

    meu primeiro commit

--- cypress/e2e/0-testes/pre-cadastro.cy.js
+++ cypress/e2e/0-testes/pre-cadastro.cy.js
@@ -1,31 +1,32 @@
 /// <reference types='cypress' />
+
 const { faker } = require('@faker-js/faker');
 
+describe("Funcionalidade Pré Cadastro", () => {
+  beforeEach(() => {
+    cy.visit("http://lojaebac.ebaconline.art.br/minha-conta/");
+  });
 
+  it("Deve completar o pré cadastro com sucesso", () => {
 
+    const nomeFaker = faker.person.firstName()
+    const sobrenomeFaker = faker.person.lastName()
+    const emailFaker = faker.internet.email(nomeFaker)
 
+    cy.get("#reg_email").type(emailFaker);
+    cy.get("#reg_password").type(faker.internet.password());
+    // cy.get('#reg_email').type('tester1000@teste.com')
+    // cy.get('#reg_password').type('tester1000@1000')
+    cy.get(":nth-child(4) > .button").click();
 
-describe('Funcionalidade Pré Cadastro', () => {
-    
-    beforeEach(() => {
-    cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
-    })
+    cy.get(".woocommerce-MyAccount-navigation-link--edit-account > a").click();
+    cy.get("#account_first_name").type(nomeFaker);
+    cy.get("#account_last_name").type(sobrenomeFaker);
+    cy.get(".woocommerce-Button").click();
 
-    it('Deve completar o pré cadastro com sucesso', () => {
-
-        cy.get('#reg_email').type(faker.internet.email())
-        cy.get('#reg_password').type(faker.internet.password())
-        // cy.get('#reg_email').type('tester1000@teste.com')
-        // cy.get('#reg_password').type('tester1000@1000')
-        cy.get(':nth-child(4) > .button').click()
-
-        cy.get('.woocommerce-MyAccount-navigation-link--edit-account > a').click()
-        cy.get('#account_first_name').type(faker.person.firstName())
-        cy.get('#account_last_name').type(faker.person.lastName())
-        cy.get('.woocommerce-Button').click()
-
-        cy.get('.woocommerce-MyAccount-content').should('contain' , 'Detalhes da conta modificados com sucesso.')
-
-
-    })
+    cy.get(".woocommerce-MyAccount-content").should(
+      "contain",
+      "Detalhes da conta modificados com sucesso."
+    );
+  });
 });
